09c282075b9d7f65215b204b3b425f59f5c8d40b
sean <mclaughlin6464@gmail.com>
2017-03-02 13:10:13 -0800

    Added a lot of efficiencies that seem to be really helping. Doing sanity checks now

--- pasta/charged_ising.py
+++ pasta/charged_ising.py
@@ -8,10 +8,13 @@ from itertools import izip, product
 import numpy as np
 from scipy.special import erfc
 from matplotlib import pyplot as plt
-#import seaborn as sns
-#sns.set()
+import seaborn as sns
+sns.set()
+#sns.set_style("white")
+#sns.set_palette(sns.color_palette("BrBG", 3))
+cmap = 'coolwarm'
 
-d = 2 #fixed in this model
+d = 3 #fixed in this model
 v = {(1,1): 5.167, (-1, -1): 5.167, (1, -1): -5.5, (-1, 1): -5.5 } #MeV
 a = 1.842e-15 #lattice spaccing in m
 a_s = 1.0 #dimensionaless smoothing length
@@ -60,7 +63,7 @@ def run_ising(N, B,xb, xp, n_steps, plot = False):
     for spin in (1,0,-1):
         site_idxs[spin] = set(tuple(idx) for idx in np.c_[(lattice == spin).nonzero()])
 
-    site_idxs['occ'] = site_idxs[1] & site_idxs[-1] #sites occupied by nucleon
+    site_idxs['occ'] = site_idxs[1] | site_idxs[-1] #sites occupied by nucleon
 
     E_0 = energy(lattice)
     if plot:
@@ -68,14 +71,17 @@ def run_ising(N, B,xb, xp, n_steps, plot = False):
     for step in xrange(n_steps):
         if step%100 == 0:
             B*=1.1
-            print step, E_0, E_0/(xb*N**3), B
+            E_0 = energy(lattice)
+            print step, E_0, E_0/(xb*N**d), B
             if plot:
                 if d == 1:
-                    im = plt.imshow(lattice.reshape((1, -1)),interpolation='none')
+                    # im = plt.imshow(lattice.reshape((1, -1)), interpolation='none')
+                    sns.heatmap(lattice.reshape((1, -1)),  cbar=True, cmap=cmap)
                 else:
-                    im = plt.imshow(lattice, interpolation='none')
-                plt.colorbar(im)
-                plt.title(r'$\beta= %e, E=%0.2f$'%(B, E_0))
+                    # im = plt.imshow(lattice, interpolation='none')
+                    sns.heatmap(lattice,cbar=True, cmap=cmap)
+                # plt.colorbar(im)
+                plt.title(r'$\beta= %e, E/A=%0.2f$' % (B, E_0/(xb*N**d)))
                 plt.pause(0.1)
                 plt.clf()
         #print step
@@ -85,44 +91,49 @@ def run_ising(N, B,xb, xp, n_steps, plot = False):
             site1, site2 = np.random.randint(0, N, size=(2,d))
             site1, site2 = tuple(site1), tuple(site2)
 
-        #make the appropriate switches in the sets
-        site_idxs[lattice[site1]].remove(site1)
-        site_idxs[lattice[site2]].remove(site2)
-
-        site_idxs[lattice[site1]].add(site2)
-        site_idxs[lattice[site2]].add(site1)
-
-        if lattice[site1] == 0:
-            #site1 is becoming occupied and site2 is becoming unoccupiated
-            site_idxs['occ'].add(site1)
-            site_idxs['occ'].remove(site2)
-        elif lattice[site2] == 0:
-            #vice versa
-            site_idxs['occ'].add(site2)
-            site_idxs['occ'].remove(site1)
-
-
-        lattice[site1], lattice[site2] = lattice[site2], lattice[site1]
         t0 = time()
-        E_f = energy(lattice)
+
+        dE = delta_energy(lattice, site1, site2)
+
         t1 = time()
         #print 'Total calc time: %.3f s'%(t1-t0)
         # if E_F < E_0, keep
         # if E_F > E_0, keep randomly given change of energies
-        if E_f >= E_0:
-            keep = np.random.uniform() < np.exp(B* (E_0 - E_f))
-        else:
-            keep = True
+        if np.random.uniform() < np.exp(B* (-dE)):
+            # make the appropriate switches in the sets
+            site_idxs[lattice[site1]].remove(site1)
+            site_idxs[lattice[site2]].remove(site2)
 
-        if keep:
-            E_0 = E_f
-        else:
+            site_idxs[lattice[site1]].add(site2)
+            site_idxs[lattice[site2]].add(site1)
+
+            if lattice[site1] == 0:
+                # site1 is becoming occupied and site2 is becoming unoccupiated
+                site_idxs['occ'].add(site1)
+                site_idxs['occ'].remove(site2)
+            elif lattice[site2] == 0:
+                # vice versa
+                site_idxs['occ'].add(site2)
+                site_idxs['occ'].remove(site1)
+
             lattice[site1], lattice[site2] = lattice[site2], lattice[site1]
+    if plot:
+        if d == 1:
+            #im = plt.imshow(lattice.reshape((1, -1)), interpolation='none')
+            sns.heatmap(lattice.reshape((1, -1)), cbar = True, cmap = cmap)
+        else:
+            #im = plt.imshow(lattice, interpolation='none')
+            sns.heatmap(lattice, cbar = True, cmap = cmap)
+        #plt.colorbar(im)
+        plt.title(r'$\beta= %e, E=%0.2f$' % (B, E_0))
+        #while True:
+        #    plt.pause(0.1)
+        plt.clf()
 
     return np.array([correlation(lattice, r) for r in xrange(1, N/2+1)])
 
 #TODO should this return tuples?
-def get_NN(site, N, d, r= 1):
+def get_NN(site, N, d, r= 1, full = False):
     '''
     The NN of the site. Will only return those UP in index (east, south, and down) to avoid double counting.
     Accounts for PBC
@@ -132,12 +143,19 @@ def get_NN(site, N, d, r= 1):
         Size of one side of the lattice
     :param d:
         dimension of the lattice
+    :param r:
+        int, optional. Distance to NN to return. Default is 1.
+    :param full:
+        bool, optional. Whether or not to return "backward" neighbors on top of "forward ones." Default is False.
     :return:
         dxd numpy array where each row corresponds to the nearest neighbors.
     '''
     mult_sites = np.r_[ [site for i in xrange(d)]]
     adjustment = np.eye(d)*r
-    return ((mult_sites+adjustment)%N).astype(int)
+    if not full:
+        return ((mult_sites+adjustment)%N).astype(int)
+    #return backwards neighbors also
+    return np.r_[((mult_sites+adjustment)%N).astype(int), ((mult_sites-adjustment+N)%N).astype(int)]
 
 def energy(lattice):
     '''
@@ -156,7 +174,25 @@ def energy(lattice):
     t2 = time()
     #print 'Vn time: %.5f s \t Vc time: %0.5f s'%(t1-t0, t2-t1)
     #print Vn, Vc
-    return Vn+Vc 
+    return Vc+Vc
+
+def delta_energy(lattice, site1, site2):
+    """
+    Calculate the change in energy from swapping these two sites. Assumes swap has not yet been performed.
+    :param lattice:
+        Lattice to calculate change on.
+    :param site1:
+        d-tuple, index of first site to swtich.
+    :param site2:
+        d-tuple, index of second site to switch
+    :return:
+        dE, the energy that would result if the sites were swapped.
+    """
+    #TODO this doesn't need E_0, just can calculate the delta.
+    dVn = delta_nucpot(lattice, site1, site2 )
+    dVc = delta_colpot(lattice, site1, site2)
+
+    return dVc+dVc
 
 
 def nuclear_potential(lattice):
@@ -183,6 +219,38 @@ def nuclear_potential(lattice):
             V += v[(spin_site, spin_nn)]
     
     return V
+
+def delta_nucpot(lattice, site1, site2):
+    '''
+    The change in energy from swapping these two sites.
+    :param lattice:
+        Lattice of (-1, 0, 1 ) sites to compute NN strong force interactions on
+    :param site1:
+        d-tuple of first site in swap
+    :param site2:
+        d-tuple of second site in swap
+    :return:
+        dV, float, change in potential
+    '''
+
+    N = lattice.shape[0]
+
+    V = 0
+    for siteA, siteB in [(site1, site2), (site2, site1)]:
+        spin_siteA = lattice[siteA]
+        spin_siteB = lattice[siteB]
+        nn = get_NN(siteA, N, d, full=True)
+        for neighbor in nn:
+            spin_nn = lattice[tuple(neighbor)]
+            if spin_nn == 0:
+                continue
+            if spin_siteA !=0:
+                V -= v[(spin_siteA, spin_nn)]
+            if spin_siteB !=0:
+                V += v[(spin_siteB, spin_nn)]
+
+    return V
+
 def coulomb_potential(lattice):
     """
     The potential from the
@@ -192,12 +260,10 @@ def coulomb_potential(lattice):
     """
     N = lattice.shape[0]
     V0 = 0 #TODO figure out the value of this
-    e = 1.6e-19 #TODO this isn't right either!
-    v0 = (e**2)/(a*N)
+    e = 1.6e-19
+    v0 = (9e9)*(e**2)/(a*N)*(6.242e12)
 
     V = V0
-    #TODO numpy where != 0
-    #TODO can keep a list of proton, neutrons and unoccupied sites
     visited_sites = set()
     dimpot_opps = 0
     while site_idxs[1]:
@@ -215,7 +281,48 @@ def coulomb_potential(lattice):
     site_idxs[1] = visited_sites
     return V
 
-#TODO precomputation
+def delta_colpot(lattice, site1, site2):
+    """
+    Change in electric potential energy from swapping the two sites.
+    :param lattice:
+        Lattice of (-1, 0, 1 ) sites to compute NN strong force interactions on
+    :param site1:
+        d-tuple, index of first site to swtich.
+    :param site2:
+        d-tuple, index of second site to switch
+    :return:
+        dV, float, the change in potential from the swap
+    """
+    N = lattice.shape[0]
+    V0 = 0  # TODO figure out the value of this
+    e = 1.6e-19
+    v0 = (9e9) * (e ** 2) / (a * N) * (6.242e12)
+
+    V = V0
+    dimpot_opps = 0
+    for site in (site1, site2):
+        t0 = time()
+        if lattice[site] == 1:
+            sign = -1
+            site_idxs[1].remove(site)
+        else:
+            sign = 1
+
+        # avoid double counting by only iterating over remaining elements
+        for neighbor in site_idxs[1]:
+            V += sign*v0 * dimensionless_potential(site, neighbor, N)
+            dimpot_opps += 1
+
+        if lattice[site] == 1: #add it back!
+            site_idxs[1].add(site)
+        t1 = time()
+
+        # print 'Col Loop Time: %.3f s'%(t1-t0)
+
+    # print dimpot_opps
+
+    return V
+
 def dimensionless_potential(site1, site2, N):
     """
 
@@ -226,7 +333,7 @@ def dimensionless_potential(site1, site2, N):
     :return:
         The value of the potential between these two sites.
     """
-    diff = tuple([(site2[i]-site1[i])for i in xrange(d)])
+    diff = tuple([float(site2[i]-site1[i])/N for i in xrange(d)])
     dist = np.sqrt(np.sum(np.array(diff)**2))
 
     #TODO i'll wanna save this so all objects have access to it.
@@ -237,6 +344,7 @@ def dimensionless_potential(site1, site2, N):
         return dim_pot_dict[round(dist,1)]
 
     s_0 = a_s*1.0/N
+    #s_0 = 0.12
     #TODO this term is basically inconsequential!
     u_sr = erfc(dist/s_0)/dist
 
@@ -244,14 +352,15 @@ def dimensionless_potential(site1, site2, N):
     for l in product(range(L_MAX), repeat = d):
         if l == tuple(0 for i in xrange(d)):
             continue
-        l2 = sum(l_i for l_i in l)
-        u_lr += (np.exp(-1*np.pi**2*s_0**2*l2)/(np.pi*l2) )*(np.exp(-2*np.pi*1j*np.dot(np.array(l), diff))).real
+        l2 = sum(l_i**2 for l_i in l)
+
+        u_lr += (np.exp(-1*np.pi**2*s_0**2*l2)/(np.pi*l2) )*(np.exp(-2*np.pi*1j*np.dot(l, diff))).real
         #print u_lr, l
 
-    #print dist, u_sr, u_lr
     u_tot = u_sr+u_lr
+
     dim_pot_dict[round(dist,1)] = u_tot
-    return u_sr + u_lr
+    return u_tot
 
 
 def magnetization(lattice):
