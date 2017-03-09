09b944640123a0e8621bac94fff0c33761c53954
Sean McLaughlin <mclaughlin6464@gmail.com>
2017-03-08 16:32:32 -0800

    added comments

--- pasta/clusters.py
+++ pasta/clusters.py
@@ -9,23 +9,33 @@ from charged_ising import get_NN, pbc_r
 
 
 def find_clusters(lattice):
+    """
+    Find clusters in the lattice
+    :param lattice:
+        np array of (-1, 0, 1) sites.
+    :return clusters, occ_set
+        clusters is a dictionary where the keys are the indicies and the values are sets containing 
+        other members of that cluster
+        occ_set is the set of all occupied sites, used in toher computations so is efficient to return.
+    """
+    #find clusters with disjoint sets
     disjoint_sets = defaultdict(set)
 
     occupied_sites = zip(*(lattice != 0).nonzero())
 
+    #begin, each site is its own cluster
     for coord in occupied_sites:
         disjoint_sets[coord].add(coord)
 
     occ_set = set(occupied_sites)
 
-    # uniques = len(occupied_sites)
-
+    #for each site, get its nn and join clusters
     for site in occupied_sites:
         nn = set(get_NN(site, lattice.shape[0], len(lattice.shape), full=True)) & occ_set
         # this is an nn that is occupied! we have structure.
         for neighbor in nn:
-
             new_set = disjoint_sets[site] | disjoint_sets[neighbor]
+            
             for ns in new_set:
                 disjoint_sets[ns] = new_set
 
@@ -33,12 +43,17 @@ def find_clusters(lattice):
 
 
 def unique_clusters(clusters):
+    """
+    Find unique clusters from a cluster dict returned from find_clusters
+    :param clusters:
+        dictionary returned from find clusters linking sites to their clusters
+    :return uniques:
+        a list of all unique clusters in clusters
+    """
     uniques = []
     all_clusters = set()
     for cluster in clusters.itervalues():
         if not all_clusters >= cluster:
-            if len(all_clusters & cluster) > 0:
-                print all_clusters & cluster
             all_clusters |= cluster
             uniques.append(cluster)
 
@@ -46,6 +61,15 @@ def unique_clusters(clusters):
 
 
 def pbc_com(cluster, lattice):
+    """
+    Center of mass in periodict boundary conditions. Found this on wikipedia.
+    :param cluster:
+        A set containing the d-tuples that comprise a unique cluster
+    :param lattice:
+        The lattice the cluster lives on. 
+    :return pbc_com
+        np array of shape (d,) with the COM of the cluster in PBC
+    """
     coords = np.array(list(cluster), dtype=float) / lattice.shape[0]
     xis, zetas = np.zeros_like(coords), np.zeros_like(coords)
     for i, c in enumerate(coords):
@@ -61,36 +85,56 @@ def pbc_com(cluster, lattice):
 
 
 def cluster_size(cluster, lattice, occ_set):
+    """
+    Radial size of cluster. Computed by calculating the average distance from boundary particles to the COM
+    :param cluster:
+        A set containing the d-tuples that comprise a unique cluster
+    :param lattice:
+        The lattice the cluster lives on. 
+    :param occ_set:
+        Set continaing all occupied site indicies in the lattice. Returned from find_clusters
+    :return avg_size
+        float, average radial size of the cluster
+    """
     COM = pbc_com(cluster, lattice)
-    # coords-=COM
 
+    d = len(lattice.shape)
+    N = lattice.shape[0]
+    
     edge_sites = []
     for site in map(tuple, cluster):
-        nn = get_NN(site, lattice.shape[0], len(lattice.shape), full=True)
+        nn = get_NN(site, N,d, full=True)
+        #not all NN are occupied; is an edge site
         if not all(neighbor in occ_set for neighbor in nn):
             edge_sites.append(site)
 
     avg_dist = np.mean(
-        [np.linalg.norm(np.array(pbc_r(site, COM, lattice.shape[0])) * lattice.shape[0]) for site in edge_sites])
+        [np.linalg.norm(np.array(pbc_r(site, COM,N )) * N) for site in edge_sites])
 
     return avg_dist
 
 
 def cluster_moments(cluster, lattice):
+    """
+    Calculate the moments of inertia of a cluster around d major axes, normalized by cluster size.
+    param cluster:
+        A set containing the d-tuples that comprise a unique cluster
+    :param lattice:
+        The lattice the cluster lives on. 
+    :return moments
+        a length d numpy array with the moments of inertia of the cluster 
+    """
     coords = np.array(list(cluster), dtype=float)
     COM = pbc_com(cluster, lattice)
-    # coords-=COM
 
     d = len(lattice.shape)
     N = lattice.shape[0]
     moments = np.zeros((d,))
 
     for i in xrange(d):
-        # moments[i] = np.sum(int(j!=i)*np.sum(pbc_r(c, com, N)**2) for j in xrange(d))
         for j in xrange(d):
             if i == j:
                 continue
             moments[i] += np.sum((pbc_r(c, COM, N)[j] * N) ** 2 for c in coords)
 
-    # return moments/(2*np.sum(moments))
     return moments / coords.shape[0]
