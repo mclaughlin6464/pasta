fe4e57c35bac272da21822e73ef8ec98aa4f150e
Sean McLaughlin <mclaughlin6464@gmail.com>
2017-03-08 16:08:20 -0800

    Update charged_ising.py
    Added comments and stuff

--- pasta/charged_ising.py
+++ pasta/charged_ising.py
@@ -7,42 +7,48 @@ import os
 from time import time
 from math import fsum
 from itertools import izip, product
-from collections import defaultdict
 import numpy as np
 from numpy.linalg import norm
 from scipy.special import erfc
-#import matplotlib
-#matplotlib.use('Agg')
+import matplotlib
+matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 import seaborn as sns
 
 sns.set()
 cmap = 'coolwarm'
 
-d = 3# fixed in this model
+d = 3 # fixed in this model
 v = {(1, 1): 5.167, (-1, -1): 5.167, (1, -1): -5.5, (-1, 1): -5.5}  # MeV
 a = 1.842e-15  # lattice spaccing in m
 a_s = 1.0  # dimensionaless smoothing length
-L_MAX = 10  # TODO try other values
+L_MAX = 10 
 
 downsample_rate = 1000
 
+#global dictionaries
 dim_pot_dict = {}  # store old results for the dimensionless potential
 
-site_idxs = {}
-
-pbc_r_dict = defaultdict(dict) # store distances
+site_idxs = {} # locations of various particle species in the lattice
 
 def run_ising(N, B_goal, xb, xp, n_steps, outputdir, plot=False):
     '''
-    Run the simulation at temperature B for n_steps
-    :param N: Int, length of one side
-    :param B: Float, inverse of temperature times boltzman constant
-    :param A: Int, The number of baryons in the simulation
+    Run the simulation for a lattice of xb and xp. Will start hot and cool to B_goal.
+    Runs for a total of n_steps.  
+    :param N: Int, length of one side of the lattice of dimension d
+    :param B_goal: Float, inverse of temperature times boltzman constant. Final goal value.
+    :param xb: float, The number of baryons in the simulation as a fraction of the nuclear saturation density
     :param xp: float, the proton fraction
     :param n_steps: int, Number of steps
-    :param plot: bool, whether or not to show a plot
-    :return: None
+    :param outputdir: str, directory to save lattice snapshots to. 
+    :param plot: bool, whether or not to show a plot. Only possible for 0 < d < 3
+    :return: energies, cvs, euclid_correlations, taxicab_correlations
+        energies, np array of floats of shape (n_steps,). The energy at every step.
+        cvs, np array of floats of shape (n_steps/downsample_rate,). Heat capacity measurements. 
+        euclid_correlations, np array of floats of shape (npoints,3). 2 pt correlations in euclidean 
+            space for protons, neutrons, and all nucleons
+        taxicab_correlations, np array of floats of shape (npoints,3). 2 pt correlations in taxicab 
+            space for protons, neutrons, and all nucleons
     '''
 
     if plot:
@@ -67,13 +73,8 @@ def run_ising(N, B_goal, xb, xp, n_steps, outputdir, plot=False):
     for spin in (1, 0, -1):
         site_idxs[spin] = set(tuple(idx) for idx in np.c_[(lattice == spin).nonzero()])
 
-    site_idxs['occ'] = site_idxs[1] | site_idxs[-1]  # sites occupied by nucleon
+    site_idxs['occ'] = site_idxs[1] | site_idxs[-1]  # sites occupied by nucleons
     E_0 = energy(lattice)
-
-    if xb == 1.0 and (xp == 0.0 or xp == 1.0): #no swaps possible, will infinite loop
-        print E_0
-        energies = np.array([E_0 for i in xrange(n_steps+2)])
-        return energies
 
     energies = np.zeros((n_steps+2,))
     energies[0] = E_0
@@ -82,6 +83,11 @@ def run_ising(N, B_goal, xb, xp, n_steps, outputdir, plot=False):
     euclid_r_avgs =  np.zeros((int(n_steps/downsample_rate+1.0), npoints, 3))
     taxicab_r_avgs = np.zeros((int(n_steps/downsample_rate+1.0), npoints, 3))
     cvs = np.zeros((int(n_steps/downsample_rate+1.0),))
+    
+    if (xb == 1.0 and (xp == 0.0 or xp == 1.0) ) or xb == 0.0: #no swaps possible, will infinite loop
+        print E_0
+        energies = np.array([E_0 for i in xrange(n_steps+2)])
+        return energies, cvs, np.zeros((npoints,3)), np.zeros((npoints,3))
 
     if plot:
         plt.ion()
@@ -92,9 +98,10 @@ def run_ising(N, B_goal, xb, xp, n_steps, outputdir, plot=False):
     B_goal_low = B0 < B_goal #if we have to go up or down
 
     B = B0
+    
     for step in xrange(n_steps+1):
 
-        if step % downsample_rate == 0:
+        if step % downsample_rate == 0: #perform downsample operations
 
             if step > n_steps / 4: #update temperature
                 if B_goal_low:
@@ -108,8 +115,7 @@ def run_ising(N, B_goal, xb, xp, n_steps, outputdir, plot=False):
                     if B < B_goal:
                         B = B_goal
 
-            #E_0 = fsum(energies)#[step]
-            energies[step] = energy(lattice)
+            energies[step] = energy(lattice) #recalibrate energy
             E_0 = energies[step]
             tf = time()
             Cv = heat_capacity(B, energies[step-n_steps/downsample_rate:step])
@@ -144,6 +150,7 @@ def run_ising(N, B_goal, xb, xp, n_steps, outputdir, plot=False):
             site1, site2 = tuple(site1), tuple(site2)
 
         energies[step+1] = energies[step]
+        #calculate energy change of proposal
         dE = delta_energy(lattice, site1, site2)
 
         # if E_F < E_0, keep
@@ -171,17 +178,15 @@ def run_ising(N, B_goal, xb, xp, n_steps, outputdir, plot=False):
 
     if plot:
         if d == 1:
-            # im = plt.imshow(lattice.reshape((1, -1)), interpolation='none')
             sns.heatmap(lattice.reshape((1, -1)), cbar=True, cmap=cmap)
         else:
-            # im = plt.imshow(lattice, interpolation='none')
             sns.heatmap(lattice, cbar=True, cmap=cmap)
-        # plt.colorbar(im)
         plt.title(r'$\beta= %e, E=%0.2f$' % (B, E_0))
-        # while True:
+        #while True:
         #    plt.pause(0.1)
         plt.clf()
 
+    #compute g(r) given the positions we've calculated
     euclid_correlations = np.zeros((npoints, 3))
     taxicab_correlations = np.zeros((npoints, 3))
     for i, particle in enumerate((-1, 1)):
@@ -196,7 +201,7 @@ def run_ising(N, B_goal, xb, xp, n_steps, outputdir, plot=False):
 
 def get_NN(site, N, d, r=1, full=False):
     '''
-    The NN of the site. Will only return those UP in index (east, south, and down) to avoid double counting.
+    The NN of the site. Will only return those UP in index (east, south, and down) to avoid double counting, unless 'full'.
     Accounts for PBC
     :param site:
         (d,) array of coordinates in the lattice
@@ -209,7 +214,7 @@ def get_NN(site, N, d, r=1, full=False):
     :param full:
         bool, optional. Whether or not to return "backward" neighbors on top of "forward ones." Default is False.
     :return:
-        dxd numpy array where each row corresponds to the nearest neighbors.
+        list of length d with d-tuples where each element corresponds to a nearest neighbor.
     '''
     mult_sites = np.r_[[site for i in xrange(d)]]
     adjustment = np.eye(d) * r
@@ -226,8 +231,6 @@ def energy(lattice):
     Calculate the energy of a lattice
     :param lattice:
         Lattice to calculate the energy on
-    :param B:
-        Float, inverse temp times boltzman factor
     :return:
         Energy of the lattice
     '''
@@ -250,9 +253,7 @@ def delta_energy(lattice, site1, site2):
     """
     dVn = delta_nucpot(lattice, site1, site2)
     dVc = delta_colpot(lattice, site1, site2)
-    #print dVn, dVc
-    #print 'd', dVc < 0
-    #print dVn,1000*dVc
+    
     return dVn +dVc
 
 
@@ -261,26 +262,25 @@ def nuclear_potential(lattice):
     Potential based on strong force interatcions
     :param lattice :
         Lattice of (-1, 0, 1) sites to compure NN strong force interactions on
-    :param B:
-        Float, Inverse temp times boltzman constant
     :return:
-        Strong force interaction energy of entire lattice at B
+        V, strong force interaction energy of entire lattice
     '''
 
     N = lattice.shape[0]
     visited_sites = set()
     V = 0
-    additions = 0
     while site_idxs['occ']:
+        #select a site to compare strong force interactions
         site = site_idxs['occ'].pop()
         visited_sites.add(site)
         spin_site = lattice[site]
+        #get NN, and keep only those that are occupied (as rest will be 0).
         nn = set(get_NN(site, N, d)) & (site_idxs['occ'] | visited_sites)
         for neighbor in nn:
             spin_nn = lattice[tuple(neighbor)]
             V += v[(spin_site, spin_nn)]
-            additions+=1
 
+    #restore the original stie_idxs
     site_idxs['occ'] = visited_sites
 
     return V
@@ -288,7 +288,7 @@ def nuclear_potential(lattice):
 
 def delta_nucpot(lattice, site1, site2):
     '''
-    The change in energy from swapping these two sites.
+    The change in nuclear potential energy from swapping these two sites.
     :param lattice:
         Lattice of (-1, 0, 1 ) sites to compute NN strong force interactions on
     :param site1:
@@ -296,11 +296,12 @@ def delta_nucpot(lattice, site1, site2):
     :param site2:
         d-tuple of second site in swap
     :return:
-        dV, float, change in potential
+        dV, float, change in nuclear potential energy
     '''
 
     N = lattice.shape[0]
 
+    #swpaping two identical sites is the same energy
     if lattice[site1] == lattice[site2]:
         return 0
 
@@ -313,6 +314,7 @@ def delta_nucpot(lattice, site1, site2):
         nn.discard(siteB) #make usre you don't count energy that's already there!
 
         for neighbor in nn:
+            #subtract energy from old site and add for new site
             spin_nn = lattice[neighbor]
             if spin_siteA != 0:
                 V -= v[(spin_siteA, spin_nn)]
@@ -324,10 +326,11 @@ def delta_nucpot(lattice, site1, site2):
 
 def coulomb_potential(lattice):
     """
-    The potential from the
+    The potential from Coulomb ineractions in the lattice. 
     :param lattice:
-        Lattice to calculate statistic on
+        Lattice to calculate energy from
     :return:
+        V, float, Coulomb potential
     """
     N = lattice.shape[0]
     e = 1.6e-19
@@ -345,27 +348,16 @@ def coulomb_potential(lattice):
         u_lr0 += np.exp(-1 * np.pi ** 2 * s_0 ** 2 * l2)
     U0+=Z/2*u_lr0
 
-    Us= []
+    Us= [] #keep a list to add in a numerically stbale way
 
     #Had to do this cuz i was getting non-deterministic behavior
     proton_list = sorted(list(site_idxs[1]), key = lambda x: x[0])
     #print len(site_idxs[1])
-    U2s = 0
     while proton_list:
         site = proton_list.pop()
-        #print site, tuple(((site[0]+1)%N, site[1]))
-        for neighbor in proton_list:
-            #print site, neighbor
-            Us.append(dimensionless_potential(site, neighbor, N) )
-#            U2 = dimensionless_potential( ((site[0]+1)%N, site[1]), ( (neighbor[0]+1)%N, neighbor[1]), N)
-            U2 = dimensionless_potential(neighbor, site, N) 
-            U2s+=U2
-#            print ((site[0]+1)%N, site[1]), ( (neighbor[0]+1)%N, neighbor[1]),U2
-            #if U2 != Us[-1]:
-            #    print 'A', site, neighbor, Us[-1], U2
 
-            #if U2s != fsum(Us):
-            #    print 'B',U2s, fsum(Us)
+        for neighbor in proton_list:
+            Us.append(dimensionless_potential(site, neighbor, N) )
 
     return v0*(fsum(Us)+U0)
 
@@ -384,8 +376,10 @@ def delta_colpot(lattice, site1, site2):
         dV, float, the change in potential from the swap
     """
     spin1, spin2 = lattice[site1], lattice[site2]
+    #swaps that generate no dV
     if (spin1, spin2) in [(0,0), (0, -1), (-1, 0), (-1,-1), (1,1)]:
         return 0
+    
     N = lattice.shape[0]
     e = 1.6e-19
     v0 = (9e9) * (e ** 2) / (a * N) * (6.242e12)
@@ -395,7 +389,7 @@ def delta_colpot(lattice, site1, site2):
     pos_site = site1 if spin1 == 1 else site2
     zero_site = site2 if spin1 == 1 else site1
 
-    site_idxs[1].remove(pos_site)
+    site_idxs[1].remove(pos_site) #make sure to not include itself as a neighbor
 
     Us = []
     #print len(site_idxs[1])
@@ -427,7 +421,7 @@ def pbc_r(site1, site2, N):
 
 def dimensionless_potential(site1, site2, N):
     """
-
+    Dimensionless Ewald potential between two points in pbc
     :param site1:
         d-tuple of a coordinate in the lattice
     :param site2:
@@ -436,11 +430,10 @@ def dimensionless_potential(site1, site2, N):
         The value of the potential between these two sites.
     """
     diff = pbc_r(site1, site2, N)
+    #TODO could I use a norm here instead?
     dist = np.sqrt(np.sum(np.array(diff) ** 2))
-    #print site1, ((site1[0] + 1) % N, site1[1]),site2, ((site2[0] + 1) % N, site2[1])
-    #print diff, dist
-    #print '_-'*50
 
+    #attempt to retrieve precomputed answer
     if round(dist, 1) in dim_pot_dict:
         return dim_pot_dict[round(dist, 1)]
 
@@ -458,7 +451,7 @@ def dimensionless_potential(site1, site2, N):
 
         u_lr += (np.exp(-1 * np.pi ** 2 * s_0 ** 2 * l2) / (np.pi * l2)) * (
         np.exp(-2 * np.pi * 1j * np.dot(l, diff))).real
-    #print site1, site2, u_lr
+
     u_tot = u_sr + u_lr
     dim_pot_dict[round(dist, 1)] = u_tot
     return u_tot
@@ -476,62 +469,24 @@ def heat_capacity(B, Es):
     """
     #Bf = 1.0/11700
     #Cv =  3/2*len(site_idxs['occ'])+np.pi**2*len(site_idxs[1])*(Bf/B)+
+    
+    #Ignore non-interaction terms
     Cv = (B**2)*Es.var()
     return Cv
 
-def _pair_correlation(particle, r, N, tol = 1e-3):
-    """
-    Correlation between two particles or spaces at a given distance.
-    :param particle:
-        type of particle. Must be among {-1, 0, 1, "occ"}, where occ is all nucleons
-    :param r:
-        float, Distance to count particles at. Must be between 0 and 1.
-    :param N:
-        int, size of the lattice
-    :param tol:
-        float, distance tolerance. Default is 1e-3
-    :return:
-        r_avg, the pair-pair correlation of this particular particle on the lattice.
-    """
-    assert particle in site_idxs
-    assert 0 <= r <= 1
-
-    r_avg = 0
-    paircount = 0
-    Np = len(site_idxs[particle])
-    visited_sites = set()
-
-    while site_idxs[particle]:
-        site1 = site_idxs[particle].pop()
-        visited_sites.add(site1)
-
-        for site2 in site_idxs[particle]:
-
-            r_vec = pbc_r(site1, site2, N)
-            r_mag = np.sqrt(sum([r_i**2 for r_i in r_vec]))
-            paircount+=1
-            if np.abs(r-r_mag) < tol:
-                r_avg += 1
-
-    site_idxs[particle] = visited_sites
-
-    if paircount == 0:
-        return 1
-
-    #return 1 + N**d/(Np*(Np-1.0))*(r_avg*1.0/paircount)
-    return r_avg
 
 def pair_correlation(particle, npoints, N, dist = 'euclidean'):
     """
-    Correlation between two particles or spaces at a given distance.
+    Correlation between two particles or spaces at a given distance. 
+    More precisely, the distance from every particle to every other of the same species, binned.
     :param particle:
         type of particle. Must be among {-1, 0, 1, "occ"}, where occ is all nucleons
-    :param r:
-        float, Distance to count particles at. Must be between 0 and 1.
+    :param npoints:
+        int , number of bins to split the distance measurment into
     :param N:
         int, size of the lattice
-    :param tol:
-        float, distance tolerance. Default is 1e-3
+    :param dist:
+        str, distance metric to use. Must be either 'eucldiean' or 'taxicab'. Default is 'euclidean'
     :return:
         r_avg, the pair-pair correlation of this particular particle on the lattice.
     """
@@ -543,6 +498,7 @@ def pair_correlation(particle, npoints, N, dist = 'euclidean'):
     normpos = np.array(list(site_idxs[particle]), dtype=float)#/N
     diffs = np.zeros( ((normpos.shape[0]*(normpos.shape[0]-1))/2, d) )
     idxs = np.triu_indices(normpos.shape[0] , 1)
+    #this hack diffs every element with every other. 
     for dim in xrange(d):
         x = np.reshape(normpos[:,dim], (len(normpos), 1))
         diffmat =  x - x.transpose()
@@ -557,7 +513,6 @@ def pair_correlation(particle, npoints, N, dist = 'euclidean'):
     else:
         pbc_dists = np.sum(pbc_diffs, axis = 1)
 
-    #normalize
     return np.histogram(pbc_dists,np.linspace(0,1*N,num=npoints+1))[0]
 
 def g_r(particle, r_avgs,N):
@@ -576,17 +531,14 @@ def g_r(particle, r_avgs,N):
     Np = len(site_idxs[particle])
     if Np == 0:
         return  1
-    #print r_avgs
-    #print r_avgs.mean()
+    
     return 1 + N**d/(Np*(Np-1.0))*(r_avgs.mean())
 
 
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Simulate an ising model')
+    parser = argparse.ArgumentParser(description='Simulate a charged ising model')
     parser.add_argument('N', type=int, help='Length of one side of the cube.')
-    parser.add_argument('B_goal', type=float, help='Bond coupling strength.')
-
+    parser.add_argument('B_goal', type=float, help='Final value of Beta for the simulation to converge to.')
     parser.add_argument('xb', type = float, default = 1.0, nargs = '?',\
                         help = 'Baryon occupation fraction. Default is 1.0')
     parser.add_argument('xp', type=float, default=0.5, nargs='?',
@@ -604,12 +556,6 @@ if __name__ == '__main__':
 
     if not os.path.isdir(args.outputdir):
         os.mkdir(args.outputdir)
-
-    energies = []
-    #xbs = [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-    #for xb in xbs:
-        #print xb
-        #energies.append(run_ising(xb=xb, **vars(args)))
 
     energies, cvs, euclid_correlations, taxicab_correlations = run_ising(**vars(args))
 
